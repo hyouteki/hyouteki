0e76ac7b5e285d4c89213f6215d29b3b65effe0c
hyouteki <lakshay21060@iiitd.ac.in>
2024-11-07 13:59:17 +0530

    add libinput-gestures.conf

--- scripts/copy_dotfiles.sh
+++ scripts/copy_dotfiles.sh
@@ -10,6 +10,7 @@ fi
 
 cp ~/.config/i3/config ${ROOT}/i3/config
 cp ~/.config/i3status/config ${ROOT}/i3status/config
+cp ~/.config/libinput-gestures.conf ${ROOT}/gestures/libinput-gestures.conf
 cp ~/.tmux.conf ${ROOT}/
 cp ~/.emacs ${ROOT}/
 cp ~/.emacs.custom.el ${ROOT}/
